ff8bd24f38bb611c965dad02f363548dcb7d74a6
vedran24r <50883077+vedran24r@users.noreply.github.com>
2019-10-10 20:55:52 +0200

    povezao bazu

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,12 @@
 package application;
 	
+
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.jcg.jdbc.mssql.JdbcMsSql;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,14 +14,25 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			JdbcMsSql.getDbConnection();
+			Statement stmt = JdbcMsSql.connObj.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Korisnik");
+			rs.next();
+			System.out.println(rs.getString("prezime"));
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/com/jcg/jdbc/mssql/JdbcMsSql.java
@@ -0,0 +1,23 @@
+package com.jcg.jdbc.mssql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JdbcMsSql {
+	public static Connection connObj;
+    public static String JDBC_URL = "jdbc:sqlserver://localhost:1433;databaseName=db_atm;integratedSecurity=true";
+ 
+    public static void getDbConnection() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connObj = DriverManager.getConnection(JDBC_URL);
+            
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+ 
+    public static void main(String[] args) {
+        getDbConnection();
+    }
+}
